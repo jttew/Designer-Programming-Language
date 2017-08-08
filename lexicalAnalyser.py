bca6b229dab94b8c52072d8c0608f599b228e6e7
jttew <jttew@crimson.ua.edu>
2017-08-08 18:00:59 -0400

    Converted tabs to spaces

--- lexicalAnalyser.py
+++ lexicalAnalyser.py
@@ -8,56 +8,56 @@ import string
 import sys
 
 class Scanner:
-	name = ''
-	#a = None, use this in funcs as a lexer obj
+    name = ''
+    #a = None, use this in funcs as a lexer obj
 
-	def __init__(self, filename):
-		self.name = filename
-		self.a = Lexer(self.name)
+    def __init__(self, filename):
+        self.name = filename
+        self.a = Lexer(self.name)
 
-	def lex(self):
-		return self.a.lex()
-			
-	def scan(self):
-		LexemeList = []
-		token = self.a.lex()
-		while(token.category != "ENDofINPUT"):
-			#print(token.category, token.value)
-			LexemeList.append(token)
-			token = self.a.lex()
-		return LexemeList
+    def lex(self):
+        return self.a.lex()
+
+    def scan(self):
+        LexemeList = []
+        token = self.a.lex()
+        while(token.category != "ENDofINPUT"):
+            #print(token.category, token.value)
+            LexemeList.append(token)
+            token = self.a.lex()
+        return LexemeList
 
 class Lexer:
-	tokens = [] #I put tokens here
-	tokenType = []
-	#tokenLineNumbers = [] #see line 94
-	currentlyString = False
+    tokens = [] #I put tokens here
+    tokenType = []
+    #tokenLineNumbers = [] #see line 94
+    currentlyString = False
 
-	def __init__(self, filename):
-		 #open and read the file	
-		op = open(filename, 'r')
-		rawLines = op.readlines()
-		op.close() 
-		lines = []
-		for line in rawLines:
-			lines += [line.strip()]
-		self.treat(lines)
+    def __init__(self, filename):
+         #open and read the file
+        op = open(filename, 'r')
+        rawLines = op.readlines()
+        op.close()
+        lines = []
+        for line in rawLines:
+            lines += [line.strip()]
+        self.treat(lines)
 
-	 #calls treatLine for each line
-	def treat(self, lines):
-		lineNum = 0
-		for line in lines:
-			self.treatLine(line, lineNum)
-			lineNum += 1
-	
-	 #finds the positions of each item		
-	def treatLine(self, line, number):
-		tokenPositions = []
-		currentlyChars = False
-		currentlyComment = False
-		if(self.currentlyString == True):
-			sys.exit()
-		for spot in range(len(line)):
+     #calls treatLine for each line
+    def treat(self, lines):
+        lineNum = 0
+        for line in lines:
+            self.treatLine(line, lineNum)
+            lineNum += 1
+
+     #finds the positions of each item
+    def treatLine(self, line, number):
+        tokenPositions = []
+        currentlyChars = False
+        currentlyComment = False
+        if(self.currentlyString == True):
+            sys.exit()
+        for spot in range(len(line)):
 #			 #comments
 #			if(line[spot] == '#'):
 #				if (self.currentlyString == False):
@@ -66,126 +66,126 @@ class Lexer:
 #				tempLine = list(line)
 #				tempLine[spot] = " "
 #				line = ''.join(tempLine)
-			 #while in string
-			if(self.currentlyString == True):
-				if (line[spot] == '"'):
-					tokenPositions += [spot + 1]
-					self.currentlyString = False
-			 ###
-			 #whtespace
-			elif(line[spot] == ' '):
-				if currentlyChars == True:
-					tokenPositions += [spot]
-					currentlyChars = False
-			 #string start
-			elif(line[spot] == '"'):
-				if currentlyChars == True:
-					tokenPositions += [spot]
-					currentlyChars = False
-				tokenPositions += [spot]
-				self.currentlyString = True
-			 #punctuation
-			elif(line[spot] in string.punctuation):
-				if currentlyChars == True:
-					tokenPositions += [spot]
-					currentlyChars = False
-				tokenPositions += [spot]
-				tokenPositions += [spot + 1]
-			 #letter or number
-			else:
-				if currentlyChars == False:
-					tokenPositions += [spot]
-					currentlyChars = True
-		 #adding the last position if it is not punctuation
-		if(len(line) > 0):
-			if(line[-1] in string.punctuation):
-				pass
-			else:
-				tokenPositions += [len(line)]
-		 #converting locations to tokens
-		tokPosLength = len(tokenPositions)
-		token = []
-		for x in range(0, tokPosLength, 2):
-			self.tokens += [line[tokenPositions[x]:tokenPositions[x+1]]]
-			#self.tokenLineNumbers += [number]
+             #while in string
+            if(self.currentlyString == True):
+                if (line[spot] == '"'):
+                    tokenPositions += [spot + 1]
+                    self.currentlyString = False
+             ###
+             #whtespace
+            elif(line[spot] == ' '):
+                if currentlyChars == True:
+                    tokenPositions += [spot]
+                    currentlyChars = False
+             #string start
+            elif(line[spot] == '"'):
+                if currentlyChars == True:
+                    tokenPositions += [spot]
+                    currentlyChars = False
+                tokenPositions += [spot]
+                self.currentlyString = True
+             #punctuation
+            elif(line[spot] in string.punctuation):
+                if currentlyChars == True:
+                    tokenPositions += [spot]
+                    currentlyChars = False
+                tokenPositions += [spot]
+                tokenPositions += [spot + 1]
+             #letter or number
+            else:
+                if currentlyChars == False:
+                    tokenPositions += [spot]
+                    currentlyChars = True
+         #adding the last position if it is not punctuation
+        if(len(line) > 0):
+            if(line[-1] in string.punctuation):
+                pass
+            else:
+                tokenPositions += [len(line)]
+         #converting locations to tokens
+        tokPosLength = len(tokenPositions)
+        token = []
+        for x in range(0, tokPosLength, 2):
+            self.tokens += [line[tokenPositions[x]:tokenPositions[x+1]]]
+            #self.tokenLineNumbers += [number]
 
-	 #pop each item and find what type it is
-	def lex(self):
-		try:
-			l = Lexeme()
-			l.token = self.tokens.pop(0)
-			punct = l.punctDict.get(l.token, 0)
-			term = l.termDict.get(l.token, 0)
-			if(punct != 0):
-				l.value = l.token
-				l.category = punct
-				return l 
-			elif(l.token.isdigit()):
-				l.value = int(l.token)
-				l.category = "INTEGER"
-				return l
-			elif(l.token[0] == '"'):
-				l.value = l.token
-				l.category = "STRING"
-				return l
-			elif(term != 0):
-				l.value = l.token
-				l.category = term
-				return l
-			else:
-				l.value = l.token
-				l.category = "ID"
-				return l
-		except IndexError: #for pop()
-			x = Lexeme()
-			x.category = "ENDofINPUT"
-			return x	
-	
+     #pop each item and find what type it is
+    def lex(self):
+        try:
+            l = Lexeme()
+            l.token = self.tokens.pop(0)
+            punct = l.punctDict.get(l.token, 0)
+            term = l.termDict.get(l.token, 0)
+            if(punct != 0):
+                l.value = l.token
+                l.category = punct
+                return l
+            elif(l.token.isdigit()):
+                l.value = int(l.token)
+                l.category = "INTEGER"
+                return l
+            elif(l.token[0] == '"'):
+                l.value = l.token
+                l.category = "STRING"
+                return l
+            elif(term != 0):
+                l.value = l.token
+                l.category = term
+                return l
+            else:
+                l.value = l.token
+                l.category = "ID"
+                return l
+        except IndexError: #for pop()
+            x = Lexeme()
+            x.category = "ENDofINPUT"
+            return x
+
 
 class Types:
-	punctDict = {
-	'(' : "OPAREN",
-	')' : "CPAREN",
-	'[' : "OBRACE", #yes i know these are brackets
-	']' : "CBRACE", 
-	'{' : "OBRACKET", #and these are braces
-	'}' : "CBRACKET",
-	'.' : "PERIOD",
-	',' : "COMMA",
-	'+' : "PLUS",
-	'-' : "MINUS",
-	'*' : "MULTIPLY",
-	'/' : "DEVIDE",
-	'^' : "POWER",
-	'%' : "MOD",
-	'<' : "LESSTHAN",
-	'>' : "GREATERTHAN",
-	'=' : "ASSIGN",
-	'!' : "NOT",
-	';' : "SEMICOLON"
-	}
-	
-	termDict = {
-	'if' : "IF",
-	'else' : "ELSE",
-	'while' : "WHILE",
-	'def' : "DEFINE",
-	'Null' : "NULL",
-	'True' : "TRUE",
-	'False' : "FALSE",
-	'class' : "CLASS",
-	'make' : "MAKE",
-	'print' : "PRINT"
-	}
+    punctDict = {
+    '(' : "OPAREN",
+    ')' : "CPAREN",
+    '[' : "OBRACE", #yes i know these are brackets
+    ']' : "CBRACE",
+    '{' : "OBRACKET", #and these are braces
+    '}' : "CBRACKET",
+    '.' : "PERIOD",
+    ',' : "COMMA",
+    '+' : "PLUS",
+    '-' : "MINUS",
+    '*' : "MULTIPLY",
+    '/' : "DEVIDE",
+    '^' : "POWER",
+    '%' : "MOD",
+    '<' : "LESSTHAN",
+    '>' : "GREATERTHAN",
+    '=' : "ASSIGN",
+    '!' : "NOT",
+    ';' : "SEMICOLON"
+    }
+
+    termDict = {
+    'if' : "IF",
+    'else' : "ELSE",
+    'while' : "WHILE",
+    'def' : "DEFINE",
+    'Null' : "NULL",
+    'True' : "TRUE",
+    'False' : "FALSE",
+    'class' : "CLASS",
+    'make' : "MAKE",
+    'print' : "PRINT"
+    }
 
 class Lexeme(Types):
-	token = 0
-	category = None
-	value = None
-	left = None
-	right = None
-	
-	
+    token = 0
+    category = None
+    value = None
+    left = None
+    right = None
+
+
 
  #defining and calling main
 #def main():
--- prettyPrint.py
+++ prettyPrint.py
@@ -6,171 +6,171 @@ import sys
 
 def prettyPrint(tree):
 
-	if(tree == None):
-		return None
+    if(tree == None):
+        return None
 
-	cat = tree.category
+    cat = tree.category
 
-	if(cat == "INTEGER"):
-		print(tree.value, end=' ')
+    if(cat == "INTEGER"):
+        print(tree.value, end=' ')
 
-	elif(cat == "ID"):
-		print(tree.value, end=' ')
+    elif(cat == "ID"):
+        print(tree.value, end=' ')
 
-	elif(cat == "STRING"):
-		print('"' + tree.value + '"', end=' ')
+    elif(cat == "STRING"):
+        print('"' + tree.value + '"', end=' ')
 
-	elif(cat == "TOPNODE"):
-		prettyPrint(tree.left)
-		prettyPrint(tree.right)
+    elif(cat == "TOPNODE"):
+        prettyPrint(tree.left)
+        prettyPrint(tree.right)
 
-	elif(cat == "CLASS"):
-		print("class " + str(tree.right.value) + " [")
-		prettyPrint(tree.left)
-		print("  ]")
+    elif(cat == "CLASS"):
+        print("class " + str(tree.right.value) + " [")
+        prettyPrint(tree.left)
+        print("  ]")
 
-	elif(cat == "FUNC"):
-		print("def", end=' ')
-		prettyPrint(tree.left)
-		print("(", end=' ')
-		t = tree.left.right
-		if(t != None):
-			prettyPrint(t)
-			t = t.right
-		while(t != None):
-			print(",", end=' ')
-			prettyPrint(t)
-			t = t.right
-		print(") [")
-		prettyPrint(tree.right)
-		print("]")
-		
-	elif(cat == "DECL"):
-		prettyPrint(tree.left)
-		print(";")
-		prettyPrint(tree.right) 
+    elif(cat == "FUNC"):
+        print("def", end=' ')
+        prettyPrint(tree.left)
+        print("(", end=' ')
+        t = tree.left.right
+        if(t != None):
+            prettyPrint(t)
+            t = t.right
+        while(t != None):
+            print(",", end=' ')
+            prettyPrint(t)
+            t = t.right
+        print(") [")
+        prettyPrint(tree.right)
+        print("]")
 
-	elif(cat == "VARDECL"):
-		print(str(tree.left.value) + " =", end=' ')
-		prettyPrint(tree.right)
+    elif(cat == "DECL"):
+        prettyPrint(tree.left)
+        print(";")
+        prettyPrint(tree.right)
 
-	elif(cat == "PRIMARY"):
-		prettyPrint(tree.left)
-		#TREE.RIGHT COVERED IN OTHER FUNCTIONS
+    elif(cat == "VARDECL"):
+        print(str(tree.left.value) + " =", end=' ')
+        prettyPrint(tree.right)
 
-	elif(cat == "PAREN"):
-		print("(", end=' ')
-		prettyPrint(tree.left)
-		print(")", end=' ')
+    elif(cat == "PRIMARY"):
+        prettyPrint(tree.left)
+        #TREE.RIGHT COVERED IN OTHER FUNCTIONS
 
-	elif(cat == "PRIMARYLIST"):
-		print("(", end=' ')
-		prettyPrint(tree.left)
-		tree = tree.left.right
-		while(tree != None):
-			print(",", end=' ')
-			prettyPrint(tree.left)
-			tree = tree.left.right
-		print(")", end=' ')
+    elif(cat == "PAREN"):
+        print("(", end=' ')
+        prettyPrint(tree.left)
+        print(")", end=' ')
 
-	elif(cat == "LIST"):
-		print("{", end=' ')
-		prettyPrint(tree.left)
-		print("}", end=' ')
+    elif(cat == "PRIMARYLIST"):
+        print("(", end=' ')
+        prettyPrint(tree.left)
+        tree = tree.left.right
+        while(tree != None):
+            print(",", end=' ')
+            prettyPrint(tree.left)
+            tree = tree.left.right
+        print(")", end=' ')
 
-	elif(cat == "OPTLIST"):
-		prettyPrint(tree.left)
-		if(tree.right != None):
-			print(",", end=' ')
-			prettyPrint(tree.right)
+    elif(cat == "LIST"):
+        print("{", end=' ')
+        prettyPrint(tree.left)
+        print("}", end=' ')
 
-	elif(cat == "OP"):
-		prettyPrint(tree.left)
-		prettyPrint(tree.right)
+    elif(cat == "OPTLIST"):
+        prettyPrint(tree.left)
+        if(tree.right != None):
+            print(",", end=' ')
+            prettyPrint(tree.right)
 
-	elif(cat == "PLUS"):
-		prettyPrint(tree.left)
-		print("+", end=' ')
+    elif(cat == "OP"):
+        prettyPrint(tree.left)
+        prettyPrint(tree.right)
 
-	elif(cat == "MINUS"):
-		prettyPrint(tree.left)
-		print("-", end=' ')
+    elif(cat == "PLUS"):
+        prettyPrint(tree.left)
+        print("+", end=' ')
 
-	elif(cat == "MULTIPLY"):
-		prettyPrint(tree.left)
-		print("*", end=' ')
+    elif(cat == "MINUS"):
+        prettyPrint(tree.left)
+        print("-", end=' ')
 
-	elif(cat == "DEVIDE"):
-		prettyPrint(tree.left)
-		print("/", end=' ')
+    elif(cat == "MULTIPLY"):
+        prettyPrint(tree.left)
+        print("*", end=' ')
 
-	elif(cat == "POWER"):
-		prettyPrint(tree.left)
-		print("^", end=' ')
+    elif(cat == "DEVIDE"):
+        prettyPrint(tree.left)
+        print("/", end=' ')
 
-	elif(cat == "LOGIC"):
-		prettyPrint(tree.left)
-		print(tree.value, end=' ')
+    elif(cat == "POWER"):
+        prettyPrint(tree.left)
+        print("^", end=' ')
 
-	elif(cat == "WHILE"):
-		print("while(", end=' ')
-		prettyPrint(tree.left)
-		print(") [")
-		prettyPrint(tree.right)
-		print("]")
+    elif(cat == "LOGIC"):
+        prettyPrint(tree.left)
+        print(tree.value, end=' ')
 
-	elif(cat == "IF"):
-		print("if(", end=' ')
-		prettyPrint(tree.left)
-		print(") [")
-		prettyPrint(tree.right.left)
-		print("]")
-		prettyPrint(tree.right.right)
-	
-	elif(cat == "ELSE"):
-		print("else[")
-		prettyPrint(tree.left)
-		print("]")
+    elif(cat == "WHILE"):
+        print("while(", end=' ')
+        prettyPrint(tree.left)
+        print(") [")
+        prettyPrint(tree.right)
+        print("]")
 
-	elif(cat == "FUNCCALL"):
-		prettyPrint(tree.left)
-		prettyPrint(tree.right)
+    elif(cat == "IF"):
+        print("if(", end=' ')
+        prettyPrint(tree.left)
+        print(") [")
+        prettyPrint(tree.right.left)
+        print("]")
+        prettyPrint(tree.right.right)
+
+    elif(cat == "ELSE"):
+        print("else[")
+        prettyPrint(tree.left)
+        print("]")
+
+    elif(cat == "FUNCCALL"):
+        prettyPrint(tree.left)
+        prettyPrint(tree.right)
 
 
 def main():
-	filename = sys.argv[1]
-	x = Recognizer(filename)
-	prettyPrint(x.getTree())
-	eval_(x.getTree())
+    filename = sys.argv[1]
+    x = Recognizer(filename)
+    prettyPrint(x.getTree())
+    eval_(x.getTree())
 
 def eval_(p):
-	if(p == None):
-		return None
-	print(p)
-	print(p.category)
-	cat = p.category
-	if(cat == "TOPNODE"):
-		eval_(p.left)
-		return eval_(p.right)
-	elif(cat == "FUNC"):
-		return eval_(p.right)
-	elif(cat == "DECL"):
-		eval_(p.left)
-		return eval_(p.right)
-	elif(cat == "PRINT"):
-		args = p.left
-		print(args)
-		while args != None:
-			prettyPrint(eval_(args.left))
-			args = args.right
-		return None
-	elif(cat == "PRIMARY"):
-		return eval_(p.left)
-	elif(cat == "STRING"):
-		return p	
-	
-	sys.exit("ERROR" + cat)
-	return None
-		
+    if(p == None):
+        return None
+    print(p)
+    print(p.category)
+    cat = p.category
+    if(cat == "TOPNODE"):
+        eval_(p.left)
+        return eval_(p.right)
+    elif(cat == "FUNC"):
+        return eval_(p.right)
+    elif(cat == "DECL"):
+        eval_(p.left)
+        return eval_(p.right)
+    elif(cat == "PRINT"):
+        args = p.left
+        print(args)
+        while args != None:
+            prettyPrint(eval_(args.left))
+            args = args.right
+        return None
+    elif(cat == "PRIMARY"):
+        return eval_(p.left)
+    elif(cat == "STRING"):
+        return p
+
+    sys.exit("ERROR" + cat)
+    return None
+
 
 main()
